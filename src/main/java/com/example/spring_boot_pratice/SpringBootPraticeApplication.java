1e119bef7940c0d7c9b55806c27a4e6e60b5306d
김대훈 <eogns136@gmail.com>
2025-12-24 15:09:30 +0900

    ADD: 첫 테스트 코드

--- src/main/java/com/example/spring_boot_pratice/SpringBootPraticeApplication.java
+++ src/main/java/com/example/spring_boot_pratice/SpringBootPraticeApplication.java
@@ -2,7 +2,9 @@ package com.example.spring_boot_pratice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class SpringBootPraticeApplication {
 
