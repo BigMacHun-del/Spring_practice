f40cf42372e0c609d0713c4550370aa13db7045b
김대훈 <eogns136@gmail.com>
2025-12-24 14:34:10 +0900

    ADD: Spring Boot 초기 세팅

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring_practice
+스프링 강의 연습 레포지토리
---
+++ src/test/java/com/example/spring_boot_pratice/SpringBootPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_pratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPraticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
